e1fe60c491928744c05005c4fd413beb66f5ac58
Thien Nguyen <thien.ngu25@gmail.com>
2025-06-12 16:25:36 +0200

    Feature : component navbar v2

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,25 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import {NgClass} from "@angular/common";
+import { Component, inject } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { map } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import { CustomBreakpoints } from '../../core/constants/breakpoints';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { effect } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [
-    NgClass
-  ],
+  imports: [NgClass, AsyncPipe],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  private breakpointObserver = inject(BreakpointObserver);
+
   menuOpen = false;
+
+  isDesktop = toSignal(
+    this.breakpointObserver
+      .observe([CustomBreakpoints.tablet])
+      .pipe(map((result) => result.matches)),
+    { initialValue: false }
+  );
+
+  constructor() {
+    effect(() => {
+      if (!this.isDesktop()) this.menuOpen = false;
+    });
+  }
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
-  }
-
-  ngOnInit() {
-    if (window.innerWidth >= 1024) {
-      this.menuOpen = false;
-    }
   }
 }
