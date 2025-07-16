39e97a7e0662076c96a8e3b05189815035cf8599
Mudy Mud <bastien.masselon@laposte.net>
2025-07-16 17:01:17 +0200

    feature : register user with form values when form is submitted

--- src/app/core/services/user-register.service.ts
+++ src/app/core/services/user-register.service.ts
@@ -9,21 +9,9 @@ import { Observable } from 'rxjs';
 export class UserRegisterService {
   private readonly http = inject(HttpClient);
 
-  private readonly baseUrl = "http://localhost:8080/magicTrade-api/auth/register";
+  private readonly _baseUrl = "http://localhost:8080/magicTrade-api/auth/register";
 
-  private readonly _userExample = {
-    "email": "ajani@example.com",
-    "password": "StrongP@ssw0rd!",
-    "pseudo": "AjaniLion",
-    "firstName": "Ajani",
-    "lastName": "Goldmane",
-    "country": "Naya",
-    "department": "Sunspire",
-    "city": "Jund City"
+  execute(user: UserRegister): Observable<object> {
+    return this.http.post<object>(this._baseUrl, user);
   }
-
-  execute(user: UserRegister = this._userExample): Observable<object> {
-    return this.http.post<object>(this.baseUrl, user);
-  }
-  
 }
