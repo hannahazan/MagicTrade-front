dd60572424bdd21fe43cc5a1fc0ae8c62e530add
Mudy Mud <bastien.masselon@laposte.net>
2025-07-17 11:49:11 +0200

    feature : create login page ; make auth container adapt to signup or login input ; make background illustration adapt to signup or login mode ; minor styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,16 @@ import { Routes } from '@angular/router';
 import { SandboxComponent } from './pages/sandbox/sandbox.component';
 import { SignUpPageComponent } from './pages/sign-up-page/sign-up-page.component';
 import {CollectionComponent} from "./pages/collection/collection.component";
+import {LoginPageComponent} from "./pages/login-page/login-page.component";
 
 export const routes: Routes = [
     {
         path: 'signup',
         component: SignUpPageComponent
+    },
+    {
+      path: 'login',
+      component: LoginPageComponent
     },
     {
         path: 'sandbox',
