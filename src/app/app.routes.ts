018a85c73b8c6489bb4ace411c267b61a2efddd7
Mudy Mud <bastien.masselon@laposte.net>
2025-08-26 16:36:19 +0200

    feat : authGuard - check if user is logged before authorizing user to access some pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import {LoginPageComponent} from "./pages/login-page/login-page.component";
 import {ProfilePageComponent} from "./pages/profile-page/profile-page.component";
 import {TradersComponent} from "./pages/traders/traders.component";
 import {WishlistComponent} from "./pages/wishlist/wishlist.component";
+import {authGuard} from "./core/guards/auth.guard";
 
 export const routes: Routes = [
     {
@@ -19,7 +20,8 @@ export const routes: Routes = [
     },
     {
       path: 'profile',
-      component: ProfilePageComponent
+      component: ProfilePageComponent,
+      canActivate: [authGuard]
     },
     {
         path: 'sandbox',
