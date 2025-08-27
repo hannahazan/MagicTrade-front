568bb45dff1ee696d398686bcf79085818fc7262
Thien Nguyen <thien.ngu25@gmail.com>
2025-08-27 15:48:20 +0200

    SCRUM 68 - Rebaseing

---
+++ src/app/pages/filters/filters.component.ts
@@ -0,0 +1,44 @@
+import {Component, inject} from '@angular/core';
+import {AuthTextInputComponent} from "../../shared/components/auth-text-input/auth-text-input.component";
+import {FormBuilder, ReactiveFormsModule} from "@angular/forms";
+import {ButtonComponent} from "../../shared/components/button/button.component";
+import {SelectComponent} from "../../shared/components/select/select.component";
+
+@Component({
+  selector: 'app-filters',
+  standalone: true,
+  imports: [
+    AuthTextInputComponent,
+    ReactiveFormsModule,
+    ButtonComponent,
+    SelectComponent
+  ],
+  templateUrl: './filters.component.html',
+  styleUrl: './filters.component.scss'
+})
+export class FiltersComponent {
+  private readonly fb = inject(FormBuilder);
+
+  searchForm = this.fb.group({
+    cardName: [''],
+    typeLine: [''],
+    colors: this.fb.group({
+      white: [false],
+      blue: [false],
+      black: [false],
+      red: [false],
+      green: [false],
+      colorless: [false],
+    }),
+    manaCost: [''],
+    set: ['']
+  });
+
+  onSubmit() {
+    console.log(this.searchForm.value);
+  }
+
+  onSortChange(value: string) {
+    console.log('Selected value:', value);
+  }
+}
