d106b2b74bcd12206bfe86acb2f21573fb447fcd
Mudy Mud <bastien.masselon@laposte.net>
2025-08-29 18:31:46 +0200

    feat : card flip looks better ; better typing and mapping for card displayed in card page

--- src/app/pages/card-page/card-page.component.ts
+++ src/app/pages/card-page/card-page.component.ts
@@ -5,6 +5,7 @@ import {NewLineToParagraphPipe} from "../../shared/pipes/new-line-to-paragraph.p
 import {CardModalComponent} from "../../shared/components/add-card-to-collection-modal/card-modal.component";
 import {TraderPreview} from "../../models/trader-preview";
 import {Router} from "@angular/router";
+import {DisplayedSingleCard, DisplayedDoubleCard} from "../../models/displayed-card";
 
 @Component({
   selector: 'app-card-page',
@@ -17,10 +18,93 @@ export class CardPageComponent implements OnInit {
 
   private readonly router = inject(Router);
 
-  displayedCard!: any;
+  displayedCard!: DisplayedSingleCard | DisplayedDoubleCard;
 
+  isFrontFace = true; // Prop seulement pour les cartes doubles
+  isAddCardModalOpen = false;
+  isCardInWishlist = false;
+
+  get currentCardFace(): keyof DisplayedDoubleCard['doubleCards'] {
+    return this.isFrontFace ? "frontFace" : "backFace";
+  }
+
+  // Mappeur à placer dans un service dans le futur
+  initDisplayedCard(card: any): void {
+    if (card.isDoubleCard) {
+      this.displayedCard = {
+        name: card.name,
+        setName: card.setName,
+        cardMarketLink: card.cardMarketLink,
+        isDoubleCard: true,
+        doubleCards: {
+          frontFace: {
+            name: card.doubleCards[0].name,
+            typeLine: card.doubleCards[0].typeLine,
+            text: card.doubleCards[0].text,
+            imageUrl: card.doubleCards[0].imageSizeNormal,
+          },
+          backFace: {
+            name: card.doubleCards[1].name,
+            typeLine: card.doubleCards[1].typeLine,
+            text: card.doubleCards[1].text,
+            imageUrl: card.doubleCards[1].imageSizeNormal,
+          }
+        },
+      };
+    } else {
+      this.displayedCard = {
+        name: card.name,
+        setName: card.setName,
+        cardMarketLink: card.cardMarketLink,
+        isDoubleCard: false,
+        typeLine: card.typeLine,
+        text: card.text,
+        imageUrl: card.imageSizeNormal,
+      };
+    }
+  }
+
+  flipCard(): void {
+    if (this.card.isDoubleCard) {
+      this.isFrontFace = !this.isFrontFace;
+    }
+  }
+
+  addToWishlist(): void {
+    // TODO : appeler un service qui ajoute la carte à la wishlist
+    this.isCardInWishlist = true;
+    console.log("card added to wishlist", this.card);
+  }
+
+  toggleAddCardModal(): void {
+    this.isAddCardModalOpen = !this.isAddCardModalOpen;
+  }
+
+  clickOnViewTraders(): void {
+     void this.router.navigate(["/traders"]);
+  }
+
+  ngOnInit(): void {
+    this.initDisplayedCard(this.card);
+  }
+
+  // Exemple de single card
+  // card: any = {
+  //   name: "Ambitious Farmhand",
+  //   setName: "Innistrad Remastered",
+  //   typeLine: "Creature — Human Peasant",
+  //   text: "When this creature enters, you may search your library for a basic Plains card, reveal it, put it into your hand, then shuffle.\nCoven — {1}{W}{W}: Transform this creature. Activate only if you control three or more creatures with different powers.",
+  //   imageSizeNormal: "https://cards.scryfall.io/normal/front/5/4/54d4e7c3-294d-4900-8b70-faafda17cc33.jpg?1634346867",
+  //   cardMarketLink: "https://www.cardmarket.com/en/Magic/Products?idProduct=574956&referrer=scryfall&utm_campaign=card_prices&utm_medium=text&utm_source=scryfall",
+  //   isDoubleCard: false,
+  //   doubleCards: []
+  // }
+
+  // Exemple de double card
   card: any = {
     name: "Ambitious Farmhand // Seasoned Cathar",
+    setName: "Innistrad Remastered",
+    cardMarketLink: "https://www.cardmarket.com/en/Magic/Products?idProduct=574956&referrer=scryfall&utm_campaign=card_prices&utm_medium=text&utm_source=scryfall",
     isDoubleCard: true,
     doubleCards: [
       {
@@ -75,63 +159,4 @@ export class CardPageComponent implements OnInit {
       ownedCardState: "excellent"
     }
   ]
-
-  private isFrontFace = true;
-  isAddCardModalOpen = false;
-  isCardInWishlist = false;
-
-  initDisplayedCard(card: any): void {
-    if (card.isDoubleCard) {
-      this.isFrontFace = true;
-      this.updateDisplayedCard();
-    } else {
-      this.displayedCard = {
-        name: card.name,
-        setName: card.setName,
-        cardMarketLink: card.cardMarketLink,
-        types: card.typeLine,
-        text: card.text,
-        imageSizeNormal: card.imageSizeNormal
-      };
-    }
-  }
-
-  flipCard(): void {
-    if (this.card.isDoubleCard) {
-      this.isFrontFace = !this.isFrontFace;
-      this.updateDisplayedCard();
-    }
-  }
-
-  private updateDisplayedCard(): void {
-    const card = this.card;
-    const face = this.isFrontFace ? card.doubleCards[0] : card.doubleCards[1];
-    this.displayedCard = {
-      name: card.name,
-      setName: card.setName,
-      cardMarketLink: card.cardMarketLink,
-      types: face.typeLine,
-      text: face.text,
-      imageSizeNormal: face.imageSizeNormal
-    };
-  }
-
-  addToWishlist(): void {
-    // TODO : appeler un service qui ajoute la carte à la wishlist
-    this.isCardInWishlist = true;
-    console.log("card added to wishlist", this.card);
-  }
-
-  toggleAddCardModal(): void {
-    this.isAddCardModalOpen = !this.isAddCardModalOpen;
-  }
-
-  clickOnViewTraders(): void {
-     void this.router.navigate(["/traders"]);
-  }
-
-  ngOnInit(): void {
-    this.initDisplayedCard(this.card);
-  }
-
 }
