d6b2c9e7608dfe21340c484230d17ec27d2e024f
Mudy Mud <bastien.masselon@laposte.net>
2025-09-23 16:28:22 +0200

    feat : create reactive form in the modal

--- src/app/shared/components/add-card-to-collection-modal/card-modal.component.ts
+++ src/app/shared/components/add-card-to-collection-modal/card-modal.component.ts
@@ -1,23 +1,52 @@
-import {Component, input, output} from '@angular/core';
+import {Component, inject, input, output} from '@angular/core';
 import {ButtonComponent} from "../button/button.component";
+import {SelectComponent} from "../select/select.component";
+import {AuthCheckboxComponent} from "../auth-checkbox/auth-checkbox.component";
+import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-add-card-to-collection-modal',
   standalone: true,
   imports: [
-    ButtonComponent
+    ButtonComponent,
+    SelectComponent,
+    AuthCheckboxComponent,
+    ReactiveFormsModule
   ],
   templateUrl: './card-modal.component.html',
   styleUrl: './card-modal.component.scss'
 })
 export class CardModalComponent {
 
+  private readonly formBuilder = inject(FormBuilder);
+
+  failedToAddCard = false;
+
+  AddCardToCollectionForm = this.formBuilder.group({
+    state: ['', [Validators.required]],
+    language: ['', [Validators.required]],
+    foil: [false, [Validators.required]],
+    fullArt: [false, [Validators.required]],
+    textless: [false, [Validators.required]]
+  })
+
   cardId = input.required<string>();
   closeModal = output<void>();
   addToCollection = output<any>();
 
-  onAdd(): void {
+  onStateChange(evt: string): void {
+    console.log(evt);
+    this.AddCardToCollectionForm.controls['state'].setValue(evt);
+  }
+
+  onLanguageChange(evt: string): void {
+    console.log(evt);
+    this.AddCardToCollectionForm.controls['language'].setValue(evt);
+  }
+
+  onSubmit(): void {
     this.addToCollection.emit(this.cardId);
-    this.closeModal.emit();
+    // this.closeModal.emit();
+    console.log(this.AddCardToCollectionForm.getRawValue());
   }
 }
