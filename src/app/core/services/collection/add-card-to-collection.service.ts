a0c6461f0f4414051b7e7161c4f44ac042a76d83
Thien Nguyen <thien.ngu25@gmail.com>
2025-11-02 18:29:29 +0100

    fix/SCRUM-106 : get collection by user Id

--- src/app/core/services/collection/add-card-to-collection.service.ts
+++ src/app/core/services/collection/add-card-to-collection.service.ts
@@ -11,15 +11,23 @@ export class AddCardToCollectionService {
   private readonly http = inject(HttpClient);
   private readonly _apiUrl = `${environment.magicTradeApiUrl}collections`;
 
+  // Ajoute une carte à la collection de l'utilisateur connecté
   execute(card: CollectionCard): Observable<void> {
     return this.http.post<void>(this._apiUrl, card);
   }
 
+  // Récupère la collection de l'utilisateur connecté
   getUserCollection(): Observable<CollectionCard[]> {
-    return this.http.get<CollectionCard[]>(`${this._apiUrl}/mycollection`);
+    return this.http.get<CollectionCard[]>(`${this._apiUrl}/MyCollection`);
   }
 
+  // Récupère la collection d'un autre utilisateur (optionnel)
+  getCollectionByUserId(userId: number): Observable<CollectionCard[]> {
+    return this.http.get<CollectionCard[]>(`${this._apiUrl}/${userId}`);
+  }
+
+  // Supprime un élément de la collection de l'utilisateur connecté
   deleteFromCollection(collectionId: number): Observable<void> {
-    return this.http.delete<void>(`${this._apiUrl}/mycollection/${collectionId}`);
+    return this.http.delete<void>(`${this._apiUrl}/MyCollection/${collectionId}`);
   }
 }
