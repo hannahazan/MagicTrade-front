4374929ca2a59a77364c187064b6e7669983a11c
hannahazan <hannahazan2@gmail.com>
2025-12-08 12:14:04 +0100

    updated user register form, removed first and last names

--- src/app/models/user-register.model.ts
+++ src/app/models/user-register.model.ts
@@ -2,8 +2,6 @@ export interface UserRegister {
   email: string;
   password: string;
   pseudo: string;
-  firstName: string;
-  lastName: string;
   country: string;
   department: string;
   city: string;
